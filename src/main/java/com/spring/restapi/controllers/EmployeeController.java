1b999e39c24c512f2cc75b49dc374fd55f58dc57
albertyosia <albertyosia77@gmail.com>
2020-03-23 14:10:38 +0700

    Initial commit

---
+++ src/main/java/com/spring/restapi/controllers/EmployeeController.java
@@ -0,0 +1,10 @@
+package com.spring.restapi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/employee")
+public class EmployeeController {
+
+}
