a90abcc2187f28bf0f34af1d50ee0c8e8140d5be
albertyosia <y0h4ne$316>
2020-04-23 17:49:21 +0700

    added spring security

---
+++ src/main/java/com/spring/restapi/security/ApplicationSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.spring.restapi.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.csrf().disable()
+      .authorizeRequests().anyRequest().authenticated()
+      .and().httpBasic();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  /**
+   * this method is used to create memory user authentication details.
+   * @param authentication for authentication details.
+   * @throws Exception .
+   */
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder authentication)
+      throws Exception {
+    authentication.inMemoryAuthentication()
+        .withUser("admin")
+        .password(passwordEncoder().encode("admin"))
+        .authorities("USER");
+  }
+}
